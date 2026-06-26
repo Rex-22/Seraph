c902a96337e4a5b68f3aebbfe9d3867ef6ef37dd
Ruben Bezuidenhout <rubenb364@gmail.com>
2026-06-26 23:17:47 +0200

    WIP Refactoring Texture2D

--- Seraph/src/Seraph/Graphics/Texture2D.h
+++ Seraph/src/Seraph/Graphics/Texture2D.h
@@ -9,6 +9,57 @@
 namespace Seraph
 {
 
+struct Texture2DCreateInfo
+{
+    enum TextureUsage
+    {
+        None = BGFX_TEXTURE_NONE,
+        MSAASample = BGFX_TEXTURE_MSAA_SAMPLE,
+        RenderTarge = BGFX_TEXTURE_RT,
+        ComputeWrite = BGFX_TEXTURE_COMPUTE_WRITE,
+        sRGB = BGFX_TEXTURE_SRGB,
+        BlitDestination = BGFX_TEXTURE_BLIT_DST,
+        ReadBack = BGFX_TEXTURE_READ_BACK,
+        ExternalShared = BGFX_TEXTURE_EXTERNAL_SHARED
+    };
+
+    enum RenderTargetOptions
+    {
+        MSAAx2 = BGFX_TEXTURE_RT_MSAA_X2,
+        MSAAx4 = BGFX_TEXTURE_RT_MSAA_X4,
+        MSAAx8 = BGFX_TEXTURE_RT_MSAA_X8,
+        MSAAx16 = BGFX_TEXTURE_RT_MSAA_X16,
+
+        WriteOnly = BGFX_TEXTURE_RT_WRITE_ONLY,
+    };
+
+    enum WrapMode
+    {
+        UMirror = BGFX_SAMPLER_U_MIRROR,
+        UClamp = BGFX_SAMPLER_U_CLAMP,
+        UBoarder = BGFX_SAMPLER_U_BORDER,
+
+        VMirror = BGFX_SAMPLER_V_MIRROR,
+        VClamp = BGFX_SAMPLER_V_CLAMP,
+        VBoarder = BGFX_SAMPLER_V_BORDER,
+
+        WMirror = BGFX_SAMPLER_W_MIRROR,
+        WClamp = BGFX_SAMPLER_W_CLAMP,
+        WBoarder = BGFX_SAMPLER_W_BORDER,
+    };
+
+    enum FilterMode
+    {
+        MinPoint = BGFX_SAMPLER_MIN_POINT,
+        MinAnisotropic = BGFX_SAMPLER_MIN_ANISOTROPIC,
+
+        MagPoint = BGFX_SAMPLER_MAG_POINT,
+        MagAnisotropic = BGFX_SAMPLER_MAG_ANISOTROPIC,
+
+        MipPoint = BGFX_SAMPLER_MIP_POINT,
+    };
+};
+
 class Texture2D
 {
 private:
