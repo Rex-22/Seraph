ee041376e711a4a2515230a873396aec4a9b8268
Ruben <rubenb364@gmail.com>
2026-06-27 00:35:06 +0200

    Improve logging system
    Files are now rotated and the logging system now properly calls shutdown

--- Seraph/src/Seraph/Core/Log.cpp
+++ Seraph/src/Seraph/Core/Log.cpp
@@ -3,13 +3,26 @@
 //
 
 #include "Log.h"
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks-inl.h>
+
+#include "Base.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 namespace Seraph
 {
 std::shared_ptr<spdlog::logger> Log::core_logger;
+std::shared_ptr<spdlog::logger> Log::app_logger;
+
+static std::shared_ptr<spdlog::logger> MakeLogger(
+  std::string name, const std::vector<spdlog::sink_ptr>& sinks, spdlog::level::level_enum lvl)
+{
+  auto logger = std::make_shared<spdlog::logger>(std::move(name), begin(sinks), end(sinks));
+  spdlog::register_logger(logger);
+  logger->set_level(lvl);
+  logger->flush_on(spdlog::level::err);
+  return logger;
+}
 
 void Log::Init()
 {
@@ -17,17 +30,20 @@ void Log::Init()
     logSinks.emplace_back(
         std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
     logSinks.emplace_back(
-        std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-            "Seraph.log", true));
+        std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+            "logs/Seraph.log", Megabytes(10), 10));
 
     logSinks[0]->set_pattern("%^[%T] %n: %v%$");
     logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+    spdlog::flush_every(std::chrono::seconds(2));
 
-    core_logger =
-        std::make_shared<spdlog::logger>("SP", begin(logSinks), end(logSinks));
-    register_logger(Log::core_logger);
-    core_logger->set_level(spdlog::level::trace);
-    core_logger->flush_on(spdlog::level::trace);
+    core_logger = MakeLogger("SP", logSinks, spdlog::level::trace);
+    app_logger = MakeLogger("APP", logSinks, spdlog::level::trace);
+}
+
+void Log::Shutdown()
+{
+    spdlog::shutdown();
 }
 
 } // namespace Core
--- Seraph/src/Seraph/Core/Log.h
+++ Seraph/src/Seraph/Core/Log.h
@@ -12,13 +12,21 @@ namespace Seraph
 struct Log
 {
     static std::shared_ptr<spdlog::logger> core_logger;
+    static std::shared_ptr<spdlog::logger> app_logger;
 
     static void Init();
+    static void Shutdown();
 };
 }
 
-#define CORE_TRACE(...) Seraph::Log::core_logger->trace(__VA_ARGS__);
-#define CORE_DEBUG(...) Seraph::Log::core_logger->debug(__VA_ARGS__);
-#define CORE_INFO(...) Seraph::Log::core_logger->info(__VA_ARGS__);
-#define CORE_WARN(...) Seraph::Log::core_logger->warn(__VA_ARGS__);
-#define CORE_ERROR(...) Seraph::Log::core_logger->error(__VA_ARGS__);
+#define CORE_TRACE(...) Seraph::Log::core_logger->trace(__VA_ARGS__)
+#define CORE_DEBUG(...) Seraph::Log::core_logger->debug(__VA_ARGS__)
+#define CORE_INFO(...) Seraph::Log::core_logger->info(__VA_ARGS__)
+#define CORE_WARN(...) Seraph::Log::core_logger->warn(__VA_ARGS__)
+#define CORE_ERROR(...) Seraph::Log::core_logger->error(__VA_ARGS__)
+
+#define APP_TRACE(...) Seraph::Log::app_logger->trace(__VA_ARGS__)
+#define APP_DEBUG(...) Seraph::Log::app_logger->debug(__VA_ARGS__)
+#define APP_INFO(...) Seraph::Log::app_logger->info(__VA_ARGS__)
+#define APP_WARN(...) Seraph::Log::app_logger->warn(__VA_ARGS__)
+#define APP_ERROR(...) Seraph::Log::app_logger->error(__VA_ARGS__)
