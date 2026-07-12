84e3241a88bfbe39a50c228f307fc689128d4595
Ruben <rubenb364@gmail.com>
2026-07-12 11:18:58 +0200

    Reformat transform component

--- Seraph/src/Seraph/Scene/Components/TransformComponent.h
+++ Seraph/src/Seraph/Scene/Components/TransformComponent.h
@@ -15,99 +15,109 @@ namespace Seraph
 {
 
 struct TransformComponent
-	{
-		glm::vec3 Translation = { 0.0f, 0.0f, 0.0f };
-		glm::vec3 Scale = { 1.0f, 1.0f, 1.0f };
-	private:
-		glm::vec3 RotationEuler = { 0.0f, 0.0f, 0.0f };
-		glm::quat Rotation = { 1.0f, 0.0f, 0.0f, 0.0f };
+{
+    glm::vec3 Translation = {0.0f, 0.0f, 0.0f};
+    glm::vec3 Scale = {1.0f, 1.0f, 1.0f};
 
-	public:
-		TransformComponent() = default;
-		TransformComponent(const glm::vec3& translation)
-			: Translation(translation)
-		{
-		}
+private:
+    glm::vec3 RotationEuler = {0.0f, 0.0f, 0.0f};
+    glm::quat Rotation = {1.0f, 0.0f, 0.0f, 0.0f};
 
-		glm::mat4 GetTransform() const
-		{
-			return glm::translate(glm::mat4(1.0f), Translation)
-				* glm::toMat4(Rotation)
-				* glm::scale(glm::mat4(1.0f), Scale);
-		}
+public:
+    TransformComponent() = default;
+    TransformComponent(const glm::vec3& translation) : Translation(translation)
+    {
+    }
 
-		void SetTransform(const glm::mat4& transform)
-		{
-			Math::DecomposeTransform(transform, Translation, Rotation, Scale);
-			RotationEuler = glm::eulerAngles(Rotation);
-		}
+    glm::mat4 GetTransform() const
+    {
+        return glm::translate(glm::mat4(1.0f), Translation) *
+               glm::toMat4(Rotation) * glm::scale(glm::mat4(1.0f), Scale);
+    }
 
-		glm::vec3 GetRotationEuler() const
-		{
-			return RotationEuler;
-		}
+    void SetTransform(const glm::mat4& transform)
+    {
+        Math::DecomposeTransform(transform, Translation, Rotation, Scale);
+        RotationEuler = glm::eulerAngles(Rotation);
+    }
 
-		void SetRotationEuler(const glm::vec3& euler)
-		{
-			RotationEuler = euler;
-			Rotation = glm::quat(RotationEuler);
-		}
+    glm::vec3 GetRotationEuler() const { return RotationEuler; }
 
-		glm::quat GetRotation() const
-		{
-			return Rotation;
-		}
+    void SetRotationEuler(const glm::vec3& euler)
+    {
+        RotationEuler = euler;
+        Rotation = glm::quat(RotationEuler);
+    }
 
-		void SetRotation(const glm::quat& quat)
-		{
-			// wrap given euler angles to range [-pi, pi]
-			auto wrapToPi = [](glm::vec3 v)
-			{
-				return glm::mod(v + glm::pi<float>(), 2.0f * glm::pi<float>()) - glm::pi<float>();
-			};
+    glm::quat GetRotation() const { return Rotation; }
 
-			auto originalEuler = RotationEuler;
-			Rotation = quat;
-			RotationEuler = glm::eulerAngles(Rotation);
+    void SetRotation(const glm::quat& quat)
+    {
+        // wrap given euler angles to range [-pi, pi]
+        auto wrapToPi = [](glm::vec3 v) {
+            return glm::mod(v + glm::pi<float>(), 2.0f * glm::pi<float>()) -
+                   glm::pi<float>();
+        };
 
-			glm::vec3 alternate1 = { RotationEuler.x - glm::pi<float>(), glm::pi<float>() - RotationEuler.y, RotationEuler.z - glm::pi<float>() };
-			glm::vec3 alternate2 = { RotationEuler.x + glm::pi<float>(), glm::pi<float>() - RotationEuler.y, RotationEuler.z - glm::pi<float>() };
-			glm::vec3 alternate3 = { RotationEuler.x + glm::pi<float>(), glm::pi<float>() - RotationEuler.y, RotationEuler.z + glm::pi<float>() };
-			glm::vec3 alternate4 = { RotationEuler.x - glm::pi<float>(), glm::pi<float>() - RotationEuler.y, RotationEuler.z + glm::pi<float>() };
+        auto originalEuler = RotationEuler;
+        Rotation = quat;
+        RotationEuler = glm::eulerAngles(Rotation);
 
-			// We pick the alternative that is closest to the original value.
-			float distance0 = glm::length2(wrapToPi(RotationEuler - originalEuler));
-			float distance1 = glm::length2(wrapToPi(alternate1 - originalEuler));
-			float distance2 = glm::length2(wrapToPi(alternate2 - originalEuler));
-			float distance3 = glm::length2(wrapToPi(alternate3 - originalEuler));
-			float distance4 = glm::length2(wrapToPi(alternate4 - originalEuler));
+        glm::vec3 alternate1 = {
+            RotationEuler.x - glm::pi<float>(),
+            glm::pi<float>() - RotationEuler.y,
+            RotationEuler.z - glm::pi<float>()};
+        glm::vec3 alternate2 = {
+            RotationEuler.x + glm::pi<float>(),
+            glm::pi<float>() - RotationEuler.y,
+            RotationEuler.z - glm::pi<float>()};
+        glm::vec3 alternate3 = {
+            RotationEuler.x + glm::pi<float>(),
+            glm::pi<float>() - RotationEuler.y,
+            RotationEuler.z + glm::pi<float>()};
+        glm::vec3 alternate4 = {
+            RotationEuler.x - glm::pi<float>(),
+            glm::pi<float>() - RotationEuler.y,
+            RotationEuler.z + glm::pi<float>()};
 
-			float best = distance0;
-			if (distance1 < best)
-			{
-				best = distance1;
-				RotationEuler = alternate1;
-			}
-			if (distance2 < best)
-			{
-				best = distance2;
-				RotationEuler = alternate2;
-			}
-			if (distance3 < best)
-			{
-				best = distance3;
-				RotationEuler = alternate3;
-			}
-			if (distance4 < best)
-			{
-				best = distance4;
-				RotationEuler = alternate4;
-			}
+        // We pick the alternative that is closest to the original value.
+        float distance0 = glm::length2(wrapToPi(RotationEuler - originalEuler));
+        float distance1 = glm::length2(wrapToPi(alternate1 - originalEuler));
+        float distance2 = glm::length2(wrapToPi(alternate2 - originalEuler));
+        float distance3 = glm::length2(wrapToPi(alternate3 - originalEuler));
+        float distance4 = glm::length2(wrapToPi(alternate4 - originalEuler));
 
-			RotationEuler = wrapToPi(RotationEuler);
-		}
+        float best = distance0;
+        if (distance1 < best) {
+            best = distance1;
+            RotationEuler = alternate1;
+        }
+        if (distance2 < best) {
+            best = distance2;
+            RotationEuler = alternate2;
+        }
+        if (distance3 < best) {
+            best = distance3;
+            RotationEuler = alternate3;
+        }
+        if (distance4 < best) {
+            best = distance4;
+            RotationEuler = alternate4;
+        }
 
-		friend class SceneSerializer;
-	};
+        RotationEuler = wrapToPi(RotationEuler);
+    }
 
-}
+
+    glm::vec3 Forward() const
+    {
+        return glm::rotate(GetRotation(), glm::vec3(0.0f, 0.0f, -1.0f));
+    }
+
+    glm::vec3 Right() const
+    {
+        return glm::rotate(GetRotation(), glm::vec3(1.0f, 0.0f,  0.0f));
+    }
+};
+
+} // namespace Seraph
